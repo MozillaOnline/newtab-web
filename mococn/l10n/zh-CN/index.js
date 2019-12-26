48e492c80364894f89f73e647d55f47cfbbeaae1
Hector Zhao <bzhao@mozilla.com>
2019-12-26 11:40:02 +0800

    Webpack: config to generate files for web hosting

---
+++ mococn/l10n/zh-CN/index.js
@@ -0,0 +1,6 @@
+/* eslint-env browser */
+export { default as geckoBrandFtl } from "../../../gecko-dev/l10n/zh-CN/brand.ftl";
+export { default as geckoBrandingsFtl } from "../../../gecko-dev/l10n/zh-CN/brandings.ftl";
+export { default as geckoNewtabFtl } from "../../../gecko-dev/l10n/zh-CN/newtab.ftl";
+
+export { default as mococnFtl } from "./mococn.ftl";
