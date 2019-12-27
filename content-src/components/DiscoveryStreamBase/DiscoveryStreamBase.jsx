589b11cf5098801e88a8f4f7f0c8136f378dbb30
Hector Zhao <bzhao@mozilla.com>
2019-12-27 18:00:01 +0800

    DSBase: section level tweaks for topstories

--- content-src/components/DiscoveryStreamBase/DiscoveryStreamBase.jsx
+++ content-src/components/DiscoveryStreamBase/DiscoveryStreamBase.jsx
@@ -13,6 +13,7 @@ import { DSTextPromo } from "content-src/components/DiscoveryStreamComponents/DS
 import { Hero } from "content-src/components/DiscoveryStreamComponents/Hero/Hero";
 import { Highlights } from "content-src/components/DiscoveryStreamComponents/Highlights/Highlights";
 import { HorizontalRule } from "content-src/components/DiscoveryStreamComponents/HorizontalRule/HorizontalRule";
+import { IS_MOCOCN_NEWTAB } from "content-src/lib/constants";
 import { List } from "content-src/components/DiscoveryStreamComponents/List/List";
 import { Navigation } from "content-src/components/DiscoveryStreamComponents/Navigation/Navigation";
 import { PrivacyLink } from "content-src/components/DiscoveryStreamComponents/PrivacyLink/PrivacyLink";
@@ -299,6 +300,11 @@ export class _DiscoveryStreamBase extends React.PureComponent {
 
     if (!topStories) {
       return null;
+    }
+
+    if (IS_MOCOCN_NEWTAB) {
+      // Remove the privacy notice in section menu
+      topStories.privacyNoticeURL = "";
     }
 
     // Extract TopSites to render before the rest and Message to use for header
